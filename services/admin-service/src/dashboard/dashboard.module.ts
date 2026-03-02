3579f32d309ea6fbc9d659f83cbe02c290313d2c
Achrefs16 <achrefs866@gmail.com>
2026-03-02 11:19:54 +0100

    chore: remove admin-service completely (deployment, service, ingress, source code)

--- services/admin-service/src/dashboard/dashboard.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DashboardController } from './dashboard.controller';
-import { PrismaService } from '../database/prisma.service';
-
-@Module({
-    controllers: [DashboardController],
-    providers: [PrismaService],
-})
-export class DashboardModule { }
